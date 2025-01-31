039a4b1d7540059027c05563d5599abbb3ac2177
yhyu13 <yohan680919@163.com>
2025-01-31 21:21:11 +0800

    Add local scripts

---
+++ local_build_cuda.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+eval "$(conda shell.bash hook)"
+conda activate llamacpp
+
+pip install -r requirements.txt
+
+CMAKE_BIN=/usr/bin/cmake
+CTEST_BIN=/usr/bin/ctest
+CMAKE_MAKE_PROGRAM=~/.local/bin/ninja
+CMAKE_C_COMPILER=/usr/bin/clang-16
+CMAKE_CXX_COMPILER=/usr/bin/clang++-16
+# CMAKE_C_COMPILER=/usr/bin/gcc-9
+# CMAKE_CXX_COMPILER=/usr/bin/g++-9
+
+# Adjust GNU version to cope with NVCC limit of gcc12
+sudo ln -sf /usr/bin/gcc-9 /usr/bin/gcc
+sudo ln -sf /usr/bin/g++-9 /usr/bin/g++
+
+rm -rf build
+
+$CMAKE_BIN -DLLAMA_CUDA=ON -DLLAMA_CUDA_F16=ON -DLLAMA_BUILD_TESTS=ON -DLLAMA_BUILD_EXAMPLES=ON \
+    -DCMAKE_C_COMPILER=$CMAKE_C_COMPILER \
+    -DCMAKE_CXX_COMPILER=$CMAKE_CXX_COMPILER \
+    -B ./build -S . "-DCMAKE_TOOLCHAIN_FILE=~/Documents/Gitrepo-My/vcpkg/scripts/buildsystems/vcpkg.cmake"
+
+$CMAKE_BIN --build ./build --config Release -j 32
+
+# Turn gcc back to 13
+sudo ln -sf /usr/bin/gcc-13 /usr/bin/gcc
+sudo ln -sf /usr/bin/g++-13 /usr/bin/g++
