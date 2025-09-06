848e3c6ad069e74c3677e6d61533292e9a01d60b
yhyu13 <yohan680919@163.com>
2025-09-06 17:46:06 +0800

    Update source to latest; Tested Qwen3 models, they are good!

--- local_server.sh
+++ local_server.sh
@@ -4,7 +4,9 @@ conda activate llamacpp
 
 #MODEL_NAME=dolphin-gguf/dolphin-2.9.1-yi-1.5-34b.Q8_0.gguf
 #MODEL_NAME=dolphin-gguf/dolphin-2.9-llama3-70b.Q4_K_M.gguf
-MODEL_NAME=DeepSeek-R1-Distill-Qwen-32B-GGUF/DeepSeek-R1-Distill-Qwen-32B-Q4_K_M.gguf
+#MODEL_NAME=DeepSeek-R1-Distill-Qwen-32B-GGUF/DeepSeek-R1-Distill-Qwen-32B-Q4_K_M.gguf
+#MODEL_NAME=Qwen/Qwen3-30B-A3B-Thinking-2507-Deepseek-v3.1-Distill-Q8_0.gguf
+MODEL_NAME=Qwen/Qwen3-30B-A3B-Instruct-2507-Q8_0.gguf
 
 NGL_NUM=9999
 #NGL_NUM=40 #9999
